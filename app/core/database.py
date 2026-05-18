b7f542e6d52de1e2b8daf1791968acc8cf972957
venkatareddy <venkatareddyg27@gmail.com>
2026-05-18 22:19:30 +0530

    added  tables and schemas

--- app/core/database.py
+++ app/core/database.py
@@ -1,1 +1,54 @@
-# database.py
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import declarative_base
+from app.core.config import settings
+
+
+# =========================================================
+# DATABASE URL
+# =========================================================
+
+DATABASE_URL = settings.DATABASE_URL
+
+
+# =========================================================
+# ENGINE
+# =========================================================
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+)
+
+
+# =========================================================
+# SESSION
+# =========================================================
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+# =========================================================
+# BASE
+# =========================================================
+
+Base = declarative_base()
+
+
+# =========================================================
+# DATABASE DEPENDENCY
+# =========================================================
+
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
